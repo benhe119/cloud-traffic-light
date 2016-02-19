0334bf742b54b98821891c2c4946bdcbe4f64be1
hartmut <hartmut.lang@gmail.com>
2016-02-19 21:04:28 +0100

    Move to SpringApplication

--- src/main/java/de/hartmut/jfs/cloud/CloudLightApplication.java
+++ src/main/java/de/hartmut/jfs/cloud/CloudLightApplication.java
@@ -2,14 +2,15 @@ package de.hartmut.jfs.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by hartmut on 13.02.16.
  */
+@SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class CloudLightApplication {
 
     @RequestMapping("/hello")
