84b79463ce3850b2d9bd173eb6c85a7a8ae6724a
hartmut <hartmut.lang@gmail.com>
2016-04-02 16:22:42 +0200

    Deploy

--- src/main/java/de/hartmut/jfs/cloud/security/WebSecurityConfig.java
+++ src/main/java/de/hartmut/jfs/cloud/security/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .sessionManagement().maximumSessions(5);
+            .anonymous();
     }
 
     @Autowired
