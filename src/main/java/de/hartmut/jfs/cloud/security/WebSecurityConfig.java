16b6a6faeebfc0928a8ed472c6d7c26905aa0cb5
hartmut <hartmut.lang@gmail.com>
2016-04-02 16:24:59 +0200

    Deploy

--- src/main/java/de/hartmut/jfs/cloud/security/WebSecurityConfig.java
+++ src/main/java/de/hartmut/jfs/cloud/security/WebSecurityConfig.java
@@ -16,7 +16,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .anonymous();
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 
     @Autowired
